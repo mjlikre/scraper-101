830d331a30e4e2430100c0b02242be8df8f9fe81
Michael's <mjlikre@gmail.com>
2020-03-19 10:11:49 -0700

    save files, commited

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
-
-const axios = require("axios");
-const cheerio = require("cheerio");
-
-
+// const axios = require("axios");
 const PORT = process.env.PORT || 3002;
+const request = require("request");
 
-
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const app = express();
-
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
@@ -17,67 +15,120 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-// Routes
 
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.lotterycritic.com/lottery-results/arizona/#results").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-    let data = []
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("table.lotteryResultsTable tbody tr").each(function(i, element) {
-      // Save an empty result object
-        var result = {
-            name: "",
-            winningNumbers : [],
-            nextDrawDate : ""
-        }
-        // Add the text and href of every link, and save them as properties of the result object
-        result.name = $(this)
-            .children("td.lotteryResultsTable-game")
-            .children('a')
-            .text();
-        
-        $(this)
-            .children("td.lotteryResultsTable-result")
-            .children('dl')
-            .children('div.lotteryResultsTable-regular')
-            .children('dd').each(function(i, element){
-                result.winningNumbers.push($(this).text())
-            });  
-
-        result.nextDrawDate = $(this)
-            .children("td.lotteryResultsTable-details")
-            .children('dl')
-            .children('div.lotteryResultsTable-details-dates')
-            .children('div.lotteryResultsTable-details-nextDraw')
-            .children('dd')
-            .children('time')
-            .text(); 
-        result.lastDrawDate = $(this)
-            .children("td.lotteryResultsTable-details")
-            .children('dl')
-            .children('div.lotteryResultsTable-details-dates')
-            .children('div.lotteryResultsTable-details-lastDraw')
-            .children('dd')
-            .children('time')
-            .text()
-        
-    //   result.link = $(this)
-    //     .children("a")
-    //     .attr("href");
-      // Create a new Article using the `result` object built from scrapin
-      data.push(result)
+app.post("/", async (req, res) => {
+  const options = {
+    method: "POST",
+    url: "https://api.shipengine.com/v1/rates",
+    headers: {
+      Host: process.env.Host,
+      "API-Key": process.env.API_Key,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      rate_options: { carrier_ids: ["se-196101", "se-196102"] },
+      service_codes:["ups_next_day_air_early_am", "fedex_priority_overnight"],
+      shipment: {
+        validate_address: "no_validation",
+        ship_to: {
+          name: "Amanda Miller",
+          phone: "555-555-5555",
+          address_line1: "2133 Parker Street Unit 1",
+          city_locality: "Berkeley",
+          state_province: "CA",
+          postal_code: "94704",
+          country_code: "US",
+          address_residential_indicator: "yes"
+        },
+        ship_from: {
+          company_name: "Example Corp.",
+          name: "John Doe",
+          phone: "111-111-1111",
+          address_line1: "1508 Walnut Street",
+          city_locality: "Berkeley",
+          state_province: "CA",
+          postal_code: "94704",
+          country_code: "US",
+          address_residential_indicator: "no"
+        },
+        packages: [{ weight: { value: 1, unit: "ounce" } }],
+      }
+    })
+  };
+  try {
+    request(options, (err, data) => {
+      if (err) throw err;
+      else {
+        const dataBody = JSON.parse(data.body);
+        console.log(dataBody.rate_response.rates);
+        res.send(dataBody.rate_response.rates);
+      }
     });
-
-    // Send a message to the client
-    res.json({data: data});
-  });
+  } catch (e) {
+    console.log(e);
+    res.send(e);
+  }
+});
+app.get("/carriers", async (req, res) => {
+  var options = {
+    method: "GET",
+    url: "https://api.shipengine.com/v1/carriers",
+    headers: {
+      Host: "api.shipengine.com",
+      "API-Key": process.env.API_Key
+    }
+  };
+  try {
+    request(options, function(error, response) {
+      if (error) throw new Error(error);
+      console.log(response.body);
+      res.send(response.body + "this is body");
+    });
+  } catch (e) {
+    res.send(e + "this is error");
+  }
 });
 
+app.get("/rates", async(req, res) => {
+  
+  var options = {
+    'method': 'POST',
+    'url': 'https://api.shipengine.com/v1/shipments/se-2102034/rates',
+    'headers': {
+      'Host': 'api.shipengine.com',
+      'API-Key': process.env.API_Key
+    }
+  };
+  request(options, function (error, response) { 
+    if (error) {
+      console.log(error)
+      return res.send(error)
+    }
+    console.log(response.body);
+    res.send(response.body)
+  });
+})
+
+app.get("/sample", async (req, res) => {
+
+  var options = {
+    'method': 'GET',
+    'url': 'https://api.shipengine.com/v1/carriers',
+    'headers': {
+      'API-Key': process.env.API_Key
+    }
+  };
+  request(options, function (error, response) { 
+    if (error) {
+      console.log(error)
+      return res.send(error)
+    }
+    console.log(response.body);
+    res.send(response.body)
+  });
+
+})
 
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT );
+  console.log("App running on port " + PORT);
 });
