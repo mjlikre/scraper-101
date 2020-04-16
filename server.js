7722d05c2c9b3d0f77f69b6f81aed8e458d310e7
Michael's <mjlikre@gmail.com>
2020-04-16 02:10:38 -0700

    done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
-
+const cors = require("cors")
 const PORT = process.env.PORT || 3002;
 
 const app = express();
@@ -9,11 +9,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://themicro.market"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 const routes = require("./routes");
 app.use(routes);
