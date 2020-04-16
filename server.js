2c96201c3b9f11367d72cf03b92f425b9aa5c4b3
Michael's <mjlikre@gmail.com>
2020-04-16 01:42:19 -0700

    done

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://themicro.market"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const routes = require("./routes");
 app.use(routes);
